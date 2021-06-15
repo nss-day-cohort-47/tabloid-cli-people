3f145278cd3e0fef167bb15faf4e1e2023f81854
bvinson23 <brandonvinson23@gmail.com>
2021-06-15 14:33:41 -0500

    journalmanager ready for Joel to takeover

---
+++ TabloidCLI/Models/Journal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TabloidCLI.Models
+{
+    public class Journal
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
