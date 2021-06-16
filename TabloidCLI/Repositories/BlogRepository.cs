002a39e5337cc5a8cfd5034ce67bfaac1457dcc1
NSPatton <greengun15@gmail.com>
2021-06-16 10:37:08 -0500

    blogs are now editable

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -73,9 +73,26 @@ namespace TabloidCLI.Repositories
                 }
             }
         }
-        public void Update(Blog entry)
+        public void Update(Blog blog)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE blog
+                                        SET Title = @title,
+                                            URL = @url2
+
+
+                                        WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+                    cmd.Parameters.AddWithValue("@id", blog.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public void Delete(int id)
         {
